b97d80dae49ece6bae8a05e130950f26735eea07
Eugen Ananin <eugen.ananin@codecamp-n.com>
2019-07-31 19:34:26 +0200

    initial project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './view/app.component';
 
 @NgModule({
   declarations: [
@@ -15,4 +15,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
